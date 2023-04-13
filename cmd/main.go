302d8e86687c169a4e4fd402bdf3ba7653dfb0ee
ASaltFishy <rongchuan_liu@sjtu.edu.cn>
2023-04-13 16:51:33 +0800

    read yaml file about pod

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v3"
+	"k8s/pkg/api/pod"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("test pr!")
+	// 解析pod的yaml配置文件
+	dataBytes, err := os.ReadFile("D:\\Homework\\K8s\\repository\\k8s\\pkg\\pod\\podConfigTest.yaml")
+	if err != nil {
+		fmt.Println("读取文件失败：", err)
+		return
+	}
+	fmt.Println("yaml 文件的内容: \n", string(dataBytes))
+	var podData pod.Pod
+	err2 := yaml.Unmarshal(dataBytes, &podData)
+	if err2 != nil {
+		fmt.Println("解析 yaml 文件失败：", err)
+	}
+	fmt.Println(podData)
+
+	// 根据配置文件创建容器
+
 }
