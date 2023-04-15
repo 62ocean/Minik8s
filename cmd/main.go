735c3747625c69a1b7ce1f95dde75958eaf10227
xcynt <Dindin_xc@sjtu.edu.cn>
2023-04-15 21:22:57 +0800

    etcd

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"k8s/pkg/etcd"
-)
+import "k8s/pkg/etcd"
 
 func main() {
-	etcd.EtcdInit()
-	etcd.Put("test", "test")
-	etcd.Put("test1/test1_1", "test1_1")
-	etcd.Put("test1/test1_2", "test1_2")
-	etcd.Put("test2/test2_1", "test2_1")
-	etcd.Put("test2/test2_2", "test2_2")
-	etcd.Put("test2/test2_3", "test2_3")
-
-	val := etcd.GetOne("test")
-	fmt.Printf("test: %s\n", val)
-
-	fmt.Printf("test1:\n")
-	var slice map[string]string
-	slice = etcd.GetDirectory("test1/")
-	for k, v := range slice {
-		fmt.Printf("%s: %s\n", k, v)
-	}
-
-	fmt.Printf("test2:\n")
-	slice = etcd.GetDirectory("test2/")
-	for k, v := range slice {
-		fmt.Printf("%s: %s\n", k, v)
-	}
-	etcd.EtcdDeinit()
+	etcd.EtcdTest()
 }
