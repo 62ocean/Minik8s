865b30ca63d92b090fdd4c0738c78b7b5188eb5c
xcynt <Dindin_xc@sjtu.edu.cn>
2023-04-15 19:51:07 +0800

    service

--- pkg/api/service/service.go
+++ pkg/api/service/service.go
@@ -10,14 +10,15 @@ func ServiceConfigTest() {
 	fmt.Println("测试 service 配置文件读取")
 	dataBytes, err := os.ReadFile("pkg/api/service/serviceConfigTest.yaml")
 	if err != nil {
-		fmt.Println("读取失败", err)
+		fmt.Println("读取文件失败:", err)
 		return
 	}
+	fmt.Println("文件内容：\n", string(dataBytes))
 	service := Service{}
 	err = yaml.Unmarshal(dataBytes, &service)
 	if err != nil {
-		fmt.Println("解析失败")
+		fmt.Println("解析yaml文件失败:", err)
 		return
 	}
-	fmt.Printf("service -> %+v\n", service)
+	fmt.Printf("解析结果：\n + service -> %+v\n", service)
 }
