60ad88139ec32c5d545bfdd0cd218f93896633c1
yangjialu <lzsyxxyjl@126.com>
2023-04-22 11:35:47 +0800

    读取replicaset.yaml+添加工具函数outputJson

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,40 @@ package main
 
 import (
 	"fmt"
-	"k8s/pkg/api/kubectl"
-	//"gopkg.in/yaml.v3"
-	//"k8s/pkg/api/pod"
-	"log"
+	"gopkg.in/yaml.v3"
+	"k8s/pkg/replicaset"
+	"k8s/pkg/tool"
 	"os"
-	"time"
 )
 
-func init() {
-	logFile, err := os.OpenFile("log/"+time.Now().Format("15_04_05")+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Println("open log file failed, err:", err)
-		return
-	}
-	log.SetOutput(logFile)
-	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
-	log.SetPrefix("[Pod]")
-}
+//func init() {
+//	logFile, err := os.OpenFile("log/"+time.Now().Format("15_04_05")+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+//	if err != nil {
+//		fmt.Println("open log file failed, err:", err)
+//		return
+//	}
+//	log.SetOutput(logFile)
+//	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+//	log.SetPrefix("[Pod]")
+//}
 
 func main() {
+	//解析replicaset.yaml
+	dataBytes, err := os.ReadFile("../pkg/replicaset/ReplicasetConfigTest.yml")
+	if err != nil {
+		fmt.Println("读取文件失败：", err)
+		return
+	}
+	var replicasetData replicaset.ReplicaSet
+	err2 := yaml.Unmarshal(dataBytes, &replicasetData)
+	if err2 != nil {
+		fmt.Println("解析 yaml 文件失败：", err)
+	}
+	tool.OutputJson("replicaset", replicasetData)
+	//fmt.Println(replicasetData)
+
 	// 解析pod的yaml配置文件
-	//dataBytes, err := os.ReadFile("pkg/api/pod/podConfigTest.yaml")
+	//dataBytes, err := os.ReadFile("../pkg/api/pod/podConfigTest.yaml")
 	//if err != nil {
 	//	fmt.Println("读取文件失败：", err)
 	//	return
@@ -48,7 +60,7 @@ func main() {
 	//}
 	//fmt.Println(podData)
 
-	kubectl.CmdExec()
+	//kubectl.CmdExec()
 
 	// 根据配置文件创建容器
 	//err = pod.CreatePod(podData)
