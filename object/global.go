5fcc6d06728f65db2a99ad8a5a6d081399e881fc
ASaltFishy <rongchuan_liu@sjtu.edu.cn>
2023-04-26 11:31:54 +0800

    修改回调函数框架

--- object/global.go
+++ object/global.go
@@ -1,9 +1,21 @@
 package object
 
 type Status int
+type EventType int
 
 const (
 	RUNNING Status = 0
 	STOPPED Status = 1
 	PENDING Status = 2
 )
+
+const (
+	CREATE EventType = 0
+	UPDATE EventType = 1
+	DELETE EventType = 2
+)
+
+type MQMessage struct {
+	EventType EventType
+	Value     string
+}
