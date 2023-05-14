327a8a68b854eef1b3a37a0f4fccd8f6660302da
yangjialu <lzsyxxyjl@126.com>
2023-05-14 19:48:54 +0800

    replicaset controller可以解析消息中的replicaset

--- pkg/controller/manager.go
+++ pkg/controller/manager.go
@@ -1,7 +1,15 @@
 package controller
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"gopkg.in/yaml.v3"
+	"k8s/object"
 	"k8s/pkg/controller/replicaset"
+	"k8s/pkg/global"
+	"k8s/pkg/util/HTTPClient"
+	"os"
 	"sync"
 )
 
@@ -22,7 +30,38 @@ func (m *manager) Start() {
 	var wg sync.WaitGroup
 	wg.Add(controllerNum)
 
+	fmt.Println("1213123")
+
 	go m.replicasetController.Start(&wg)
+
+	// test: add a replicaset to apiserver
+	// --------------------------------------
+	dataBytes, err := os.ReadFile("test/ReplicasetConfigTest.yml")
+	if err != nil {
+		fmt.Println("读取文件失败：", err)
+		return
+	}
+	//fmt.Println("aaaaaa")
+	var replicasetData object.ReplicaSet
+	err2 := yaml.Unmarshal(dataBytes, &replicasetData)
+	if err2 != nil {
+		fmt.Println("解析 yaml 文件失败：", err)
+	}
+	//err = utils.OutputJson("解析yaml: replicaset", replicasetData)
+	if err != nil {
+		fmt.Println("解析yaml: replicaset失败")
+		return
+	}
+	id, _ := uuid.NewUUID()
+	replicasetData.Metadata.Uid = id.String()
+	var rsJson []byte
+	rsJson, _ = json.Marshal(replicasetData)
+	//fmt.Println("rsJson: \n" + string(rsJson))
+
+	client := HTTPClient.CreateHTTPClient(global.ServerHost)
+	client.Post("/replicasets/create", rsJson)
+	fmt.Println("add replicaset ok!")
+	//--------------------------------------
 
 	// 等待所有协程执行完毕
 	wg.Wait()
@@ -30,6 +69,8 @@ func (m *manager) Start() {
 
 func NewManager() Manager {
 	m := &manager{}
+
+	fmt.Print("qqq")
 
 	// 创建各种controller
 	m.replicasetController = replicaset.NewController()
--- pkg/global/global.go
+++ pkg/global/global.go
@@ -2,3 +2,4 @@ package global
 
 var MQHost = "amqp://guest:guest@localhost:5672/"
 var EtcdHost = "localhost:2379"
+var ServerHost = "http://127.0.0.1:8080"
