37c59b4f94e49b32c39e66ec1d02bb4a9ddb735b
ASaltFishy <rongchuan_liu@sjtu.edu.cn>
2023-06-01 21:32:04 +0800

    add test files

--- pkg/kubectl/getCmd.go
+++ pkg/kubectl/getCmd.go
@@ -15,6 +15,25 @@ func GetCmd() *cli.Command {
 		Usage: "get the running information of a pod or a service",
 		Subcommands: []*cli.Command{
 			{
+				Name:  "nodes",
+				Usage: "get the running information of all node",
+				Action: func(c *cli.Context) error {
+					response := APIClient.Get("/nodes/getAll")
+					var nodeList map[string]string
+					_ = json.Unmarshal([]byte(response), &nodeList)
+					fmt.Println("NAME\t\t\tSATUS\t\t\tAGE")
+					for _, val := range nodeList {
+						nodeStorage := object.NodeStorage{}
+						_ = json.Unmarshal([]byte(val), &nodeStorage)
+						createTime := nodeStorage.Node.Metadata.CreationTimestamp
+						newtime := time.Now()
+						d := newtime.Sub(createTime)
+						fmt.Printf("%s\t\t\t%s\t\t\t%s\n", nodeStorage.Node.Metadata.Name, nodeStorage.Status.ToString(), d.Truncate(time.Second).String())
+					}
+					return nil
+				},
+			},
+			{
 				Name:  "pod",
 				Usage: "get the running information of a pod",
 				Action: func(c *cli.Context) error {
---
+++ test/HTTPGoServer/server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/emicklei/go-restful/v3"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
+
+var IP string
+
+func main() {
+	//get ip
+	ip, err := GetOutBoundIP()
+	if err != nil {
+		fmt.Println(ip)
+	}
+	IP = ip
+	// HTTP server
+	wsContainer := restful.NewContainer()
+	wsContainer.Router(restful.CurlyRouter{})
+	nodeWS := new(restful.WebService)
+	nodeWS.Path("/hello").
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+	nodeWS.Route(nodeWS.GET("/").To(Greet))
+	wsContainer.Add(nodeWS)
+
+	// run
+	server := &http.Server{Addr: ":8888", Handler: wsContainer}
+	defer server.Close()
+	log.Fatal(server.ListenAndServe())
+}
+
+func Greet(request *restful.Request, response *restful.Response) {
+	ret := "hello from " + IP + "!"
+	response.Write([]byte(ret))
+}
+
+func GetOutBoundIP() (ip string, err error) {
+	conn, err := net.Dial("udp", "8.8.8.8:53")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	fmt.Println(localAddr.String())
+	ip = strings.Split(localAddr.String(), ":")[0]
+	return
+}
